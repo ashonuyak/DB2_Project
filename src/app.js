ef8e38a09ff28f70255a56ea4b9e3da0de163479
ashonuyak <ashonuyak@gmail.com>
2021-10-22 18:47:55 +0300

    viacheslav ukrainskyi

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ router.use('/', globalRouter.router.routes());
 app.use(router.routes());
 app.use(serve(path.join(__dirname, '/')));
 
-const PORT = 3000;
+const PORT = process.env.PORT | 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
