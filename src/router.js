10f7981bc1540a7748291e617c39da8351ef4e23
ashonuyak <ashonuyak@gmail.com>
2021-10-22 18:37:16 +0300

    viacheslav ukraini

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ const Router = require("koa-router"),
 
 const router = new Router();
 
-router.get("home", Controller.profile);
+router.get("", Controller.profile);
 router.get("forgot-password", Controller.forgotPassword);
 router.get("check-email", Controller.checkEmail);
 router.get("signUpName", Controller.signUpName);
